fdb8de3d0ab308cf4008b078efb3ff1a611e4c44
Rahul-on-git <2020.rahul.motwani@ves.ac.in>
2021-12-27 14:33:58 +0530

    to open website on same zoom-scale on all size of display devices

--- script.js
+++ script.js
@@ -30,3 +30,10 @@ else
 {
     body.classList.remove('dark');
 }
+
+// code to open website on same zoom-scale on all size of display devices
+var scale = 'scale(1)';
+document.body.style.webkitTransform =  scale;    // Chrome, Opera, Safari
+ document.body.style.msTransform =   scale;       // IE 9
+ document.body.style.transform = scale;     // General
+ 
