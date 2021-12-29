3a0f78b088d8793021361cf9259f5551f93499e5
Rahul-on-git <2020.rahul.motwani@ves.ac.in>
2021-12-30 02:50:40 +0530

    linked all htmls to stylesheet of calc ie, styleC.css and inserted js for calc into script.js

--- script.js
+++ script.js
@@ -34,4 +34,34 @@ else
 // code to open website on same zoom-scale on all size of display devices
 var scale = 'scale(1)';
  document.body.style.transform = scale;     // General
+
+ //code for calculator
+
+ let display = document.getElementById('displayC');
+
+let buttons = Array.from(document.getElementsByClassName('buttonC'));
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = "Error"
+                }
+                break;
+            case '←':
+                if (display.innerText){
+                   display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
  
