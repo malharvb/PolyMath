f6a24565b85879f11b3045882b7e6d3c6c59697b
malhar <malharkajale@gmail.com>
2022-03-06 15:36:32 +0530

    added basic animation flow

--- script2.js
+++ script2.js
@@ -7,8 +7,10 @@ let arr = step2.split('<br>')
 console.log(arr)
 
 let steps = document.querySelector('#vis-display')
-arr.forEach(e => 
-    {
-        setTimeout(()=>{steps.innerHTML+= e +'<br>'}, 5000)
-    }
-    )
+setTimeout(()=>{steps.innerHTML += arr[0] +'<br>'}, 2000)
+setTimeout(()=>{steps.innerHTML += arr[1] +'<br>'}, 4000)
+setTimeout(()=>{steps.innerHTML += arr[2] +'<br>'}, 6000)
+
+
+
+//setInterval(()=>{arr.forEach(e=>{steps.innerHTML += e +'<br>'})}, 2000)
